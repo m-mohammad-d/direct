5c26b730a58696e7bba2ca2e1843f50be3345c8d
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-12-03 23:13:00 +0330

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="text-orange-500">
+      <Text className="font-bold text-5xl text-orange-500">Direct Apps</Text>
+    </View>
+  );
+}
