3d41e35dad4b05ffc4395230c9a60d99125bf246
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-12-03 23:35:29 +0330

    feat : add login page

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://YOUR-PROJECT.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_KEY";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
